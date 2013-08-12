52d988e5e3ac9d741e02ee79c676a89bffa05ab7
JD Maresco <jdmaresco@gmail.com>
2013-08-11 23:24:35 -0700

    Update User model validations and tests through 6.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase } 	# this is a callback, which is a method
+													# that gets invoked at a particular point
+													# in the lifetime of an ActiveRecord object
+
+	# name is what is being validated, final argument is a hash without brackets
+	validates :name, presence: true, length: {maximum: 50}
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 				# Name starts with a capital letter, thus this is a constant and cannot change
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+
 end
