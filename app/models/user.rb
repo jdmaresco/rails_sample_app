c94deac2ec4860e2b8cd1e559f8bca0b07118df5
JD Maresco <jdmaresco@gmail.com>
2013-09-05 21:48:54 -0700

    Model users and create sign up form through Fig 7.13

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
+	has_secure_password
 	before_save { email.downcase! } 	# this is a callback, which is a method
-													# that gets invoked at a particular point
-													# in the lifetime of an ActiveRecord object
+										# that gets invoked at a particular point
+										# in the lifetime of an ActiveRecord object
 
 	# name is what is being validated, final argument is a hash without brackets
 	validates :name, presence: true, length: {maximum: 50}
@@ -9,6 +10,5 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i 				# Name starts with a capital letter, thus this is a constant and cannot change
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
 
-	has_secure_password
 	validates :password, length: { minimum: 6 }
 end
