f53cf19f1dfb56c820b53b62160a6e0362667ca1
JD Maresco <jdmaresco@gmail.com>
2013-09-08 23:10:17 -0700

    Complete tut through end of 8.1.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,12 @@ class SessionsController < ApplicationController
 	end 
 
 	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			#sign in and redirect
+		else
+			#error msg and rerender
+		end
 	end
 	
 	def destroy
