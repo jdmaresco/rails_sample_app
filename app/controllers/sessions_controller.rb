0932c7aabac8b2f197096c71cf0d138cf9120ac3
JD Maresco <jdmaresco@gmail.com>
2013-09-11 15:30:20 -0700

    Complete basic authentication support

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,16 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			#sign in and redirect
+			sign_in user
+			redirect_to user
 		else
-			#error msg and rerender
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
 		end
 	end
 	
 	def destroy
+		sign_out
+		redirect_to root_url
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	before_save { email.downcase! } 	# this is a callback, which is a method
+	before_save { self.email.downcase! } 	# this is a callback, which is a method
 										# that gets invoked at a particular point
 										# in the lifetime of an ActiveRecord object
+	before_create :create_remember_token
 
 	# name is what is being validated, final argument is a hash without brackets
 	validates :name, presence: true, length: {maximum: 50}
@@ -11,4 +12,18 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
 
 	validates :password, length: { minimum: 6 }
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+		def create_remember_token
+			self.remember_token = User.encrypt(User.new_remember_token)
+		end
 end
