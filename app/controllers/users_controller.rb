2c800a4786651cc56b2d004689b7fff772fe8430
JD Maresco <jdmaresco@gmail.com>
2013-09-14 23:27:07 -0700

    Authentication in progress, through Listing 9.22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,  only: [:index, :edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
 
+  def index
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -14,7 +19,7 @@ class UsersController < ApplicationController
       sign_in @user
   		flash[:success] = "Welcome to the Sample App"
 		redirect_to @user
-	else
+    else
   		render 'new'
   	end
   end
@@ -38,4 +43,17 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
 
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
